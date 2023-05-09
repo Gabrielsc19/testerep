1cd06f5765a8536df388d681ecdf34a13e8c07b4
Gabrielsc19 <gabriel1903.sc@gmail.com>
2023-05-09 16:50:36 -0300

    projetoteste

---
+++ ProjetoMercado/src/br/com/Produto.java
@@ -0,0 +1,63 @@
+package br.com;
+
+public class Produto {
+	
+	private long codigo;
+	private String descricao;
+	private int quantidade;
+	private double valor;
+	
+
+	public Produto() {}
+	
+	public Produto (long codigo, String descrição, int quantidade, double valor) {
+	this.codigo = codigo;
+	this.descricao = descrição;
+	this.quantidade = quantidade;
+	this.valor = valor;
+	}
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public String consultar(int codigo) {
+		String consulta = "";
+		
+		consulta += "\n ----------------------";
+		consulta += "\n Código: "+ this.codigo;
+	    consulta += "\n Descrição: "+ this.descricao;
+		consulta += "\n Quantidade: "+ this.quantidade;
+	    consulta += "\n Valor: "+ this.valor;
+		consulta += "\n----------------------";
+		return consulta;
+	}
+}
